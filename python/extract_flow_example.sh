239f2d0164ec2ca445080c13e8d049bd139e032c
valterlej <valter_estevam@hotmail.com>
2020-02-05 14:24:54 -0300

    updates fev-05

--- python/extract_flow_example.sh
+++
@@ -1,2 +0,0 @@
-# extract only flow_x and flow_y... images are discarded
-python extract-flow.py -g True -f /tmp/flow/test.avi -x /tmp/flow/flow_x -y /tmp/flow/flow_y -i /dev/null -b 20 -t 1 -d 0 -s 1 -o=zip
